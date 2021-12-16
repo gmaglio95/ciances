2cd4c605d5af34917a9147ecf54ef380a236a67a
Giuseppe Maglio <giuseppe.maglio@hotmail.com>
2021-12-16 14:45:42 +0100

    Add About Content page custom

--- OxerREACT_JS/App/src/blocks/about/AboutContent.js
+++ OxerREACT_JS/App/src/blocks/about/AboutContent.js
@@ -4,24 +4,27 @@ const AboutContent = () => {
     return (
         <div id="about" className="block">
             <h2>
-                <span className="line">A passionate</span><br/> photographer, designer in media.
+                <span className="line">A passionate</span><br /> photographer, designer in media.
             </h2>
 
             <div className="row bg-half-ring-left gutter-width-lg">
                 <div className="col align-self-top pl-0">
                     <div className="img object-fit">
                         <div className="object-fit-cover">
-                            <img src="assets/img/placeholder/527x600.jpg" alt="Luca Reynolds" className="img-fluid" />
+                            <img src="/assets/img/profilo.png" alt="Gabriele Ciances" className="img-fluid" />
                         </div>
                     </div>
                 </div>
 
                 <div className="col align-self-center description">
-                    <h4>I’m Luca Reynolds</h4>
+                    <h4>Gabriele Ciances</h4>
 
-                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                    <p>Regista e sceneggiatore, ha iniziato a lavorare come assistente alla regia su svariati set di spot web e televisivi. Ha diretto un corto, diversi spot per il web, videoclip di noti artisti della scena musicale italiana e ha collaborato con aziende e agenzie come Bianca Film, Il Fatto Quotidiano, Huawei Honor, Vodafone, Rizzoli Editore, KIKO Milano, BluKids, OVS, Alfa Romeo, STYLE Magazine, NeroGiardini.
+                        Ha lavorato come assistente di Fabio Mollo, partecipando ad alcuni dei suoi progetti tra cui il film Il padre d'Italia (2017) con Luca Marinelli e Isabella Ragonese.</p>
 
-                    <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+                    <p>Nel 2018 insieme a Federico Allocca e Andrea Sestu, firma la scrittura e la co-regia del documentario #OPS - L'Evento, distribuito al cinema da Notorious Pictures.
+
+                    </p>
                 </div>
             </div>
         </div>
