9ccb07bc0b88abce8275f542c2b9c8e2b5ca05d1
Giuseppe Maglio <giuseppe.maglio@hotmail.com>
2022-03-15 17:02:57 +0100

    esternalizzati json modificabili

--- OxerREACT_JS/App/src/blocks/footer/FooterMenu.js
+++ OxerREACT_JS/App/src/blocks/footer/FooterMenu.js
@@ -1,27 +1,44 @@
-import React from 'react';
-import dataFooterMenu from '../../data/footer/footerMenu';
+import React, { Component } from 'react';
 
-const FooterMenu = () => {
-    return (
-        <nav className="menu-secondary">
-            <ul className="clearfix list-unstyled">
-                { dataFooterMenu && dataFooterMenu.map( ( item, key ) => {
-                    return (
-                        <li key={ key }>
-                            <a
-                                title={ item.title }
-                                className="btn btn-link transform-scale-h border-0 p-0"
-                                href={ item.link }
-                                target={'_blank'}
-                            >
-                                { item.title }
-                            </a>
-                        </li>
-                    );
-                } ) }
-            </ul>
-        </nav>
-    );
-};
+class FooterMenu extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataFooterMenu: []
+        };
+    }
+
+    componentDidMount() {
+        fetch(process.env.PUBLIC_URL + "/assets/content/footer/footerMenu.json").then(response => {
+            return response.json();
+        }).then(data => {
+            this.setState({ dataFooterMenu: data });
+        });
+    }
+
+    render() {
+        return (
+            <nav className="menu-secondary">
+                <ul className="clearfix list-unstyled">
+                    {this.state.dataFooterMenu.map((item, key) => {
+                        return (
+                            <li key={key}>
+                                <a
+                                    title={item.title}
+                                    className="btn btn-link transform-scale-h border-0 p-0"
+                                    href={item.link}
+                                    target={'_blank'}
+                                >
+                                    {item.title}
+                                </a>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </nav>
+        );
+    }
+
+}
 export default FooterMenu;
