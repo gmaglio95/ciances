8173a06b64f913d71955bace5384a046e08f9b5d
Alessandro Nizi <alessandro.nizi@aubay.it>
2022-03-19 11:54:26 +0100

    Changed Bio image

--- OxerREACT_JS/App/src/blocks/about/AboutContent.js
+++ OxerREACT_JS/App/src/blocks/about/AboutContent.js
@@ -5,12 +5,16 @@ const AboutContent = () => {
         <div id="about" className="block spacer m-top-lg">
             <div id="bio"><h4>BIO</h4></div>
 
-            <div className="row gutter-width-lg">
-                <div className="col-lg-1"></div>
-                <div className="col-lg-5 align-center about-image">
+            <div className="row gutter-width-lg bio-margin-bottom">
+                <div className="col-lg-5"></div>
+                <div className="col-lg-2 align-center about-image">
                     <img src={process.env.PUBLIC_URL + "/assets/img/about-image.jpg"} alt={process.env.PUBLIC_URL + "assets/img/about-image.jpg"} />
                 </div>
-                <div className="col-lg-5 align-self-center description text-align-center">
+            </div>
+
+            <div className="row gutter-width-lg">
+                <div className="col-lg-3"></div>
+                <div className="col-lg-6 align-self-center description text-align-center">
 
                 <p>Gabriele Ciances (1993) Regista e Filmmaker</p>
                 <p>Sono nato a Messina, dove ho avuto modo  di coltivare la mia passione per il cinema sin dall'adolescenza.</p>
